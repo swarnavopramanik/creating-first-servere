341bd15fa3765e328af8d462d04bd7661b73d924
swarnavopramanik <105142693+swarnavopramanik@users.noreply.github.com>
2022-08-28 14:09:43 +0530

    modified:   server.js

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ const http = require('http')
 
 const port = 8081; // local port no
 
+
+
 http
   .createServer(( request,response) => {   // callback function
-    response.writeHead(200, {"Content-Type": "text/html"});
+    response.writeHead
+    (200, {"Content-Type": "text/html"});
     response.write("<h1>Hello, this is from my first server</h1>");
     response.end();
 })
